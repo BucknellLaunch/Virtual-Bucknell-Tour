c91e39a0004d7d54e54f0836aa932f0e84e74b93
Kuree <xmlc001@gmail.com>
2014-01-24 19:22:23 -0500

    First Person Controller
    Initial commit.

---
+++ Assets/Standard Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour {
+
+	// Charactor controller to move
+	CharacterController characterController;
+
+	// Move
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 5.0f;
+	public float jumpSpeed = 20.0f;
+	
+	float verticalRotation = 0;
+	public float upDownRange = 60.0f;
+	
+	float verticalVelocity = 0;
+
+
+
+	// Rotation
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	
+	public float minimumY = -90F;
+	public float maximumY = 90F;
+	
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
+	
+	void Start () {
+		// Lock cursor and get cc from camera
+		Screen.lockCursor = true;
+		characterController = GetComponent<CharacterController>();
+
+		// Make the rigid body not change rotation
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	
+	void Update () {
+		// Movement
+		float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+		
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+		
+		if( characterController.isGrounded && Input.GetButton("Jump"))
+		{
+			verticalVelocity = jumpSpeed;
+		}
+		else
+		{
+			verticalVelocity = 0;	// Need to be removed once we have terrian
+		}
+		
+		
+		Vector3 speed = new Vector3( sideSpeed, verticalVelocity, forwardSpeed );
+		
+		speed = transform.rotation * speed;
+		
+		characterController.Move( speed * Time.deltaTime );
+
+
+		// Rotation
+		// Read the mouse input axis
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		
+		rotationX = ClampAngle (rotationX, minimumX, maximumX);
+		rotationY = ClampAngle (rotationY, minimumY, maximumY);
+		
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+		
+		transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		
+	}
+	
+	private static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
